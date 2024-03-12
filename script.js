74772a02bd71a3658d43247fc99b2d6f7a9cc36c
dgrow9 <100497354+dgrow9@users.noreply.github.com>
2024-03-11 19:22:28 -0700

    Erase tie statement.  Create an if else statement that checks if the player picks rock and all the options for the computer

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ function getComputerChoice(Rock, Paper, Scissors) {
 }
 
 function playRound (playerSelection, computerSelection) {
-    if ((playerSelection === "rock" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "paper") || 
-    (playerSelection === "scissors" && computerSelection === "scissors")) {
-        console.log("It's a tie!");
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You lose! Paper beats Rock!";
+        }
+        else if (computerSelection === "scissors") {
+            return "You win! Rock beats scissors!";
+        }
+        else if (computerSelection === "rock") {
+            return "It's a Tie! You both chose rock!";
+        }
+        else {
+            return "There's been an error.  Try again.";
+        }
     }
+    
 }
